465e103eb12de63d484d9c8f9e033393293cf9cd
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2024-03-08 02:29:10 +0800

    Learn: View and Assets

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,14 +1,22 @@
 <?php
 
-class About
+class About extends Controller
 {
     public function index($nama = 'Geraldi', $pekerjaan = 'Mahasiswa')
     {
-        echo "Saya $nama, seorang $pekerjaan";
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
 
     public function page()
     {
-        echo 'About/page';
+        $data['judul'] = 'page';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
